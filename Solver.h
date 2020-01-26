815750521e0fe71b3866068ce87330d0bab6aeaa
LalivT <59091636+LalivT@users.noreply.github.com>
2020-01-26 20:32:36 +0200

    Update Solver.h

--- Solver.h
+++ Solver.h
@@ -5,9 +5,18 @@
 #ifndef EX4_SOLVER_H
 #define EX4_SOLVER_H
 template <typename P, typename S>
+/**
+ * solve a problem.
+ */
 class Solver {
 public:
+    /**
+     * solve a problem P.
+     */
     virtual S solve(P) = 0;
+    /**
+     * return a deep clone of solver object.
+     */
     virtual Solver* clone() = 0;
 };
 #endif //EX4_SOLVER_H
