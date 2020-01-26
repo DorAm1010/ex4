f2c1b214bcfc974e4eead413c8b9c6225e6e12b3
LalivT <59091636+LalivT@users.noreply.github.com>
2020-01-26 19:56:56 +0200

    Update ClientHandler.h

--- ClientHandler.h
+++ ClientHandler.h
@@ -2,9 +2,19 @@
 #define EX4_CLIENTHANDLER_H
 
 template <typename T, typename S>
+/**
+ * Incharge of communication protocol with the client.
+ */
 class ClientHandler {
 public:
+    /**
+     * need to execute communication protocol,
+     * until the end of connection.
+     */
     virtual void handleClient(int) = 0;
+    /**
+     * creating a deep clone of client handler object.
+     */
     virtual ClientHandler* clone() = 0;
 };
 #endif //EX4_CLIENTHANDLER_H
